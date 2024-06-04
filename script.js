66941b36a5be90056515715689dc6727c298c9a4
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-06-04 11:48:10 -0300

    ok

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    "Pergunta 1",
-    "Pergunta 2"
-];
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
+
 

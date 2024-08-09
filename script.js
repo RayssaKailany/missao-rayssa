04f76677f1526a3fe307c69ce7c88d56177a8fa6
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-08-09 14:18:36 -0300

    ok

--- script.js
+++ script.js
@@ -30,19 +30,29 @@ function mostraPergunta() {
 }
 
 mostraAlternativas();
-
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", function(){
-                atual++;
-                mostraPergunta();
-            })
-            caixaAlternativas.appendChild(botaoAlternativas);
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+function respostaSelecionada(opcaoSelecionada){
+    atual++;
+    mostraPergunta();
+}
 
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();
 
 
